4f09828de68f7a7caa5d821b9baf0f908fec4c29
VieLongT <vielongt52@gmail.com>
2022-09-17 19:01:29 +0700

    Add project files.

---
+++ Canvas/MyColumn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canvas
+{
+    public class MyColumn
+    {
+        Point top;
+        Point bottom;
+
+        public Point Top { get => top; set => top = value; }
+        public Point Bottom { get => bottom; set => bottom = value; }
+    }
+}
